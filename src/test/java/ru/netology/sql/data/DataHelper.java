f0136977864a24f77d66e88e88b4a23c9fac5a13
Dina-72 <amenova1109@yandex.ru>
2023-09-25 16:53:46 +0500

    initial commit

---
+++ src/test/java/ru/netology/sql/data/DataHelper.java
@@ -0,0 +1,43 @@
+package ru.netology.sql.data;
+
+import com.github.javafaker.Faker;
+import lombok.Value;
+
+import java.util.Locale;
+
+public class DataHelper {
+    private static final Faker faker = new Faker(new Locale("en"));
+
+    private DataHelper() {
+    }
+
+    public static AuthInfo getAuthInfoWithTestData() {
+        return new AuthInfo("vasya", "qwerty123");
+    }
+
+    private static String generateRandomLogin() { //генерация случайного логина
+        return faker.name().username();
+    }
+
+    private static String generateRandomPassword() { //генерация случайного пароля
+        return faker.internet().password();
+    }
+
+    public static AuthInfo generateRandomUser() { //генерация случайного пользователя
+        return new AuthInfo(generateRandomLogin(), generateRandomPassword());
+    }
+
+    public static VerificationCode generateRandomVerificationCode() { //генерация случайного кода
+        return new VerificationCode(faker.numerify("######"));
+    }
+    @Value
+    public static class AuthInfo {
+        String login;
+        String password;
+    }
+
+    @Value
+    public  static class VerificationCode {
+        String code;
+    }
+}
